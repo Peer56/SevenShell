1e8adac193889ca646e4d517b0e3a44435e4a6c9
Peer56 <35853414+Peer56@users.noreply.github.com>
2024-10-10 14:41:43 +0300

    Aid icon hack finished

--- ASM/Infrastructure/EffectivenessRework/GetAidIconFromClassType.s
+++ ASM/Infrastructure/EffectivenessRework/GetAidIconFromClassType.s
@@ -1,6 +1,5 @@
 .thumb
 
-push {r1}
 ldr r0,[r6,#0xC]
 ldr r1,[r0,#0x4]
 mov r0,#0x50
@@ -10,7 +9,7 @@ mov r0,#0x1 @ horse type
 and r0,r1
 cmp r0,#0x0
 beq Dragon
-mov r0,#0xA4 @ horse icon ID
+mov r0,#0xAD @ horse icon ID
 b End
 
 Dragon:
@@ -18,7 +17,7 @@ mov r0,#0x2 @ dragon type
 and r0,r1
 cmp r0,#0x0
 beq Flier
-mov r0,#0xA5 @ dragon icon ID
+mov r0,#0xAE @ dragon icon ID
 b End
 
 Flier:
@@ -26,7 +25,7 @@ mov r0,#0x4 @ flier type
 and r0,r1
 cmp r0,#0x0
 beq Armor
-mov r0,#0xA6 @ flier icon ID
+mov r0,#0xAF @ flier icon ID
 b End
 
 Armor:
@@ -34,7 +33,7 @@ mov r0,#0x8 @ armor type
 and r0,r1
 cmp r0,#0x0
 beq Sword
-mov r0,#0xA7 @ armor icon ID
+mov r0,#0xB0 @ armor icon ID
 b End
 
 Sword:
@@ -42,7 +41,7 @@ mov r0,#0x10 @ sword type
 and r0,r1
 cmp r0,#0x0
 beq Magic
-mov r0,#0xA8 @ sword icon ID
+mov r0,#0xB1 @ sword icon ID
 b End
 
 Magic:
@@ -50,7 +49,7 @@ mov r0,#0x20 @ magic type
 and r0,r1
 cmp r0,#0x0
 beq Infantry
-mov r0,#0xA9 @ magic icon ID
+mov r0,#0xB2 @ magic icon ID
 b End
 
 Infantry:
@@ -58,13 +57,12 @@ mov r0,#0x40 @ infantry type
 and r0,r1
 cmp r0,#0x0
 bne TypeEnd
-mov r0,#0x0
-neg r0,r0 @ don't know why we need this whole part but it's in vanilla
+mov r0,#0x1
+neg r0,r0 @ return -1 if no class type
 b End
 
 TypeEnd:
-mov r0,#0xA9 @ infantry icon ID
+mov r0,#0xB3 @ infantry icon ID
 
 End:
-pop {r1}
 bx r14
